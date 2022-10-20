c6bc65da9ba5a1ef15537e8b5e5e6de28d952e18
jcardenasv <jcardenasv@unal.edu.co>
2022-10-20 18:03:17 -0500

    Solve

--- pregunta.py
+++ pregunta.py
@@ -10,6 +10,7 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
@@ -17,5 +18,46 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+
+    df = open('clusters_report.txt', 'r')
+    line1 = re.sub("\s{3,}", "  ", df.readline().strip()).split("  ")
+    line2 = df.readline().replace("\n", "").strip().split("  ")
+
+    for i in range(len(line1)):
+        line1[i] = (line1[i].strip().lower()).replace(" ", "_")
+        if i == 1 or i == 2:
+            line1[i] = (line1[i] + ' ' + line2[i-1].lower()
+                        ).replace(" ", "_")
+    df.readline(), df.readline()
+    doc = df.readlines()
+    cont = []
+    texto = ''
+
+    for line in doc:
+        line = re.sub(r"\s{2,}", " ", line.strip()).replace('\n', '')
+        line += ' '
+        if '%' in line:
+            if texto != '':
+                aux = cont.pop()
+                texto = texto.replace('.', '').strip()
+                aux[3] = aux[3] + texto
+                cont.append(aux)
+                texto = ''
+            indice = line.index('%')
+            sublista = line[:indice].strip().replace(',', '.').split(" ")
+            cont.append(sublista + [line[indice + 2:]])
+        else:
+            texto += line
+
+    aux = cont.pop()
+    texto = texto.replace('.', '').strip()
+    aux[3] = aux[3] + texto
+    cont.append(aux)
+    df = pd.DataFrame(cont, columns=line1)
+    df['cluster'] = df['cluster'].astype('int64')
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(
+        'int64')
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(
+        'float64')
 
     return df
